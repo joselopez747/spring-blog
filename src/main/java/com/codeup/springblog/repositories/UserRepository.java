eecfc94c181e4f50cccf9f28c5c61a331c61e584
Jose Lopez <jose.m.lopez747@gmail.com>
2022-05-11 17:09:12 -0500

    modified repositories for post and user

--- src/main/java/com/codeup/springblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/springblog/repositories/UserRepository.java
@@ -1,7 +1,9 @@
-//package com.codeup.springblog.repositories;
+package com.codeup.springblog.repositories;
 //
-//import com.codeup.springblog.models.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
+import com.codeup.springblog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 //
-//public class UserRepository extends JpaRepository <User,Long> {
-//}
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
