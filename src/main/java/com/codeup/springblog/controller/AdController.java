1635471961cf56c5dbc96311cfe591dc693f7239
Jose Lopez <jose.m.lopez747@gmail.com>
2022-05-18 10:49:47 -0500

    ads

---
+++ src/main/java/com/codeup/springblog/controller/AdController.java
@@ -0,0 +1,43 @@
+package com.codeup.springblog.controllers;
+
+        import com.codeup.springblog.models.Ad;
+        import com.codeup.springblog.repositories.AdRepository;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/ads")
+public class AdController {
+    private final AdRepository adDao;
+
+    public AdController(AdRepository adDao){
+        this.adDao = adDao;
+    }
+
+    @GetMapping
+    public String allAds(Model model){
+        model.addAttribute("ads", adDao.findAll());
+        return "ads/index";
+    }
+
+    @GetMapping("/{id}")
+    public String showAnAd(@PathVariable long id, Model model){
+        Ad ad = adDao.findById(id);
+        model.addAttribute("ad", adDao.findById(id));
+        return "ads/show";
+    }
+
+    @GetMapping("/search")
+    public String adSearch(){
+        return "ads/search";
+    }
+
+    @PostMapping("/search")
+    public String searchResults(@RequestParam(name = "title") String title, Model model){
+//        model.addAttribute("results", adDao.findByTitle(title));
+        model.addAttribute("results", adDao.searchByTitleLike(title));
+        return "ads/search";
+    }
+
+}
