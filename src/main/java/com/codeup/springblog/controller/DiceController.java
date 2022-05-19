845253ce0171914b36849831742d981e48fdbc36
Jose Lopez <jose.m.lopez747@gmail.com>
2022-05-19 09:58:26 -0500

    updating relationship-practice branch

--- src/main/java/com/codeup/springblog/controller/DiceController.java
+++ src/main/java/com/codeup/springblog/controller/DiceController.java
@@ -1,35 +1,35 @@
-package com.codeup.springblog.controller;
-
-
-import com.codeup.springblog.models.Dice;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Random;
-
-@Controller
-@RequestMapping("/roll-dice")
-public class DiceController {
-    private final int[] rolls = {1, 2, 3, 4, 5, 6};
-
-
-    @GetMapping
-    public String diceInfo() {
-        return "views-lecture/dice";
-    }
-
-
-    @GetMapping("/{n}")
-    @ResponseBody
-    public String randomRoll(@PathVariable int n, Model model) {
-        int rnd = new Random().nextInt(rolls.length);
-        return "Sorry you rolled a: " + rolls[rnd];
-
-
-    }
-}
+//package com.codeup.springblog.controller;
+//
+//
+//import com.codeup.springblog.models.Dice;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.Random;
+//
+//@Controller
+//@RequestMapping("/roll-dice")
+//public class DiceController {
+//    private final int[] rolls = {1, 2, 3, 4, 5, 6};
+//
+//
+//    @GetMapping
+//    public String diceInfo() {
+//        return "views-lecture/dice";
+//    }
+//
+//
+//    @GetMapping("/{n}")
+//    @ResponseBody
+//    public String randomRoll(@PathVariable int n, Model model) {
+//        int rnd = new Random().nextInt(rolls.length);
+//        return "Sorry you rolled a: " + rolls[rnd];
+//
+//
+//    }
+//}
 
